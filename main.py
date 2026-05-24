48bb5a2e22162b9d8c89415eed79415e334b5a8d
cadilac1 <155693139+cadilac1@users.noreply.github.com>
2026-05-24 00:57:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ while True:
     # check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print("quitting...")
             pygame.quit() # close window
             quit() # end pygame
 
