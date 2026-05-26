bf99c1e598be96ec17e7ecc0331ebdbb09522f66
lucas <firminolucas007@gmail.com>
2026-05-25 23:28:20 -0300

    blé

--- code/MENU2.py
+++ code/MENU2.py
@@ -15,8 +15,8 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shoote", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "Mountain", C_WHITE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shoote", C_WHITE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 self.menu_text(17, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 180 + 35 * i))
